dc33d09d753e09e59f0988338527113ffdbd2dc2
Brooklyn <58751447+bbrooklyn@users.noreply.github.com>
2022-05-16 21:36:03 +0100

    PEP 8 convention & Suitable exceptions

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
-class array:
-    def __init__ (self,size,items):
+class Array:
+    def __init__ (self, size, items):
         self.size = size
-        if list(map(type,items)).count(list(map(type,items))[0]) == len(items) and self.size>len(items):
+        if list(map(type,items)).count(list(map(type,items))[0]) == len(items) and self.size > len(items):
             self.typey = list(map(type,items))[0]
             self.array = items
         else:
-            raise Exception("TypeError: item data types do not match")
-    def append(self,item):
+            raise TypeError("Item data types do not match")
+            
+    def append(self, item):
         if type(item) == self.typey and len(self.array) < self.size:
             self.array.append(item)
         elif len(self.array) < self.size:
-            raise Exception("BufferError: Overflow error")
+            raise OverflowError("BufferError: Overflow error")
         else:
-            raise Exception("TypeError: item data type does not match")
+            raise TypeError("TypeError: item data type does not match")
    
-    def __getitem__ (self,index):
+    def __getitem__ (self, index):
         return (self.array[int(index)])
     
-    def __setitem__(self, index,item):
+    def __setitem__(self, index, item):
         if type(item) == self.typey:
             self.array[index] = item
         else:
-            raise Exception("TypeError: item data type does not match")
-
-
+            raise TypeError("TypeError: item data type does not match")
 
     def __repr__(self):
         return (self.array)
