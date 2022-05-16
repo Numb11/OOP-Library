8f5007c51c8e15f4a414d13109dc33620cd12e23
Soup <87945079+Numb11@users.noreply.github.com>
2022-05-16 19:12:26 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+class array:
+    def __init__ (self,size,items):
+        self.size = size
+        if list(map(type,items)).count(list(map(type,items))[0]) == len(items) and self.size>len(items):
+            self.typey = list(map(type,items))[0]
+            self.array = items
+        else:
+            raise Exception("TypeError: item data types do not match")
+    def append(self,item):
+        if type(item) == self.typey and len(self.array) < self.size:
+            self.array.append(item)
+        elif len(self.array) < self.size:
+            raise Exception("BufferError: Overflow error")
+        else:
+            raise Exception("TypeError: item data type does not match")
+   
+    def __getitem__ (self,index):
+        return (self.array[int(index)])
+    
+    def __setitem__(self, index,item):
+        if type(item) == self.typey:
+            self.array[index] = item
+        else:
+            raise Exception("TypeError: item data type does not match")
+
+
+
+    def __repr__(self):
+        return (self.array)
